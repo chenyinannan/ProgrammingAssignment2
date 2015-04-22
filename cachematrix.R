5a6bc22c0d3de27df87435279ee840848c844b83
chenyinannan <chenyinannan@hotmail.com>
2015-04-22 22:58:10 +0800

    programming assignment first submission
    overwrite cacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCahcheMatrix function: creates a special "Matrix" object that can cache its inverse
+## It will result a list containing a list of function to 
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of the inverse matrix
+## 4. get the value of the inverse matrix
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invMat <- NULL
+  setMat <- function (y) {
+    x<<-y
+    invMat <<- NULL   
+  }
+  getMat <- function () x 
+  setInvMat<- function (inv) invMat <<- inv
+  getInvMat <- function () invMat
+  list (setMat = setMat, getMat = getMat,
+        setInvMat = setInvMat, getInvMat = getInvMat)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve function computes the inverse of the special "matrix" returned by makeCacheMatrix
+## If the inverse has already been calculated, then the cachesolve should retrieve the inverse
+## matrix from cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  invMat <- x$getInvMat()
+  if (!is.null(invMat)) {
+    message ("getting the cached data !")
+    return (invMat)
+  }
+  invMat <- solve(x$getMat())
+  x$setInvMat(invMat)
+  invMat
 }
